da79d96e5c1cc2a1bd6536f1dbad087df0d1fe09
Filip Lindvall <drpytho@gmail.com>
2016-09-27 14:43:04 +0200

    Added keyboard and <CTRL>-z support

--- main.py
+++ main.py
@@ -138,6 +138,8 @@ class Drawer(object):
         self.__window = Gtk.Window()
         w = self.__window
         w.connect('delete-event', Gtk.main_quit)
+        w.add_events(Gdk.EventMask.KEY_PRESS_MASK)
+        w.connect("key-press-event", self.on_keyboard)
         draw_area = Gtk.DrawingArea()
         draw_area.add_events(Gdk.EventMask.BUTTON_PRESS_MASK |
                              Gdk.EventMask.BUTTON_RELEASE_MASK |
@@ -156,7 +158,6 @@ class Drawer(object):
 
     def on_draw(self, widget, ctx):
         # Draw the stack
-        print("Drawing")
         for drawable in self.__doli:
             drawable.draw(ctx)
 
@@ -169,17 +170,32 @@ class Drawer(object):
     def on_mouse_release(self, widget, event):
         # print("Release", event.x, event.y)
         ret = self.__action.on_mouse_release(Coords(event.x, event.y),
-                                       event.button == 1, event.button == 3)
+                                             event.button == Gdk.BUTTON_PRIMARY,
+                                             event.button == Gdk.BUTTON_SECONDARY)
         if (ret is not None):
             self.__doli.append(ret)
-            self.__action = DrawPolygon(self.__fg, self.__bg, True)
+            self.__action = DrawPolygon(self.__primary, self.__secondary, True)
 
         self.__draw_area.queue_draw()
 
     def on_mouse_press(self, widget, event):
-        # print("Press", event.x, event.y)
-        self.__action.on_mouse_release(Coords(event.x, event.y),
-                                       event.button == 3, event.button == 1)
+        self.__action.on_mouse_press(Coords(event.x, event.y),
+                                     event.button == Gdk.BUTTON_PRIMARY,
+                                     event.button == Gdk.BUTTON_SECONDARY)
+
+    def on_keyboard(self, widget, event):
+        if (event.keyval == Gdk.KEY_Escape):
+            # Kill the current action
+            self.__action = self.__selected_action(self.__primary, self.__secondary, True)
+
+        if (event.keyval == Gdk.KEY_q):
+            Gtk.main_quit()
+
+        if (event.keyval == Gdk.KEY_z and event.state & Gdk.ModifierType.CONTROL_MASK):
+            print("<CTRL>z")
+            if (len(self.__doli) != 0):
+                del self.__doli[-1]
+        self.__draw_area.queue_draw()
 
 
 def main():
