d717cd5d5551c724885dfdf08919dedab6391d5f
Filip Lindvall <drpytho@gmail.com>
2016-09-27 19:59:17 +0200

    Fixed some keyboard comands and Now You Can Draw Circles

--- src/drawables.py
+++ src/drawables.py
@@ -1,4 +1,7 @@
-from util import Color, Coords
+import math
+
+
+# Abtracts
 class Action(object):
     def on_mouse_release(self, coord, right, left):
         raise NotImplemented()
@@ -22,6 +25,9 @@ class Action(object):
 class Drawable(object):
     def draw(self, context):
         raise NotImplemented()
+
+
+# Shapes
 
 class Polygon(Drawable):
 
@@ -62,6 +68,25 @@ class Line(Drawable):
         ctx.stroke()
 
 
+class Circle(Drawable):
+
+    def __init__(self, prim, alt, fill, center=None, rad=None):
+        self.color = prim
+        self.center = center
+        self.rad = rad
+        self.fill = fill
+
+    def draw(self, ctx):
+        ctx.set_source_rgba(*self.color.value)
+        ctx.set_line_width(2)
+        ctx.arc(self.center.x, self.center.y, self.rad, 0, 2*math.pi)
+        if (self.fill):
+            ctx.fill()
+        else:
+            ctx.stroke()
+
+
+# Drawing tools
 class DrawLine(object):
 
     def __init__(self, primColor, altColor, fill):
@@ -75,8 +100,7 @@ class DrawLine(object):
                 return self.save()
 
     def on_mouse_press(self, coord, right, left):
-        if (left):
-            self.line.coords = []
+        pass
 
     def on_mouse_move(self, coord):
         self.tmp_coords = coord
@@ -119,5 +143,40 @@ class DrawPolygon(Action):
 
     def on_mouse_press(self, coord, right, left):
         pass
+
+
+class DrawCircle(object):
+
+    def __init__(self, primColor, altColor, fill):
+        self.circle = Circle(primColor, altColor, fill)
+        self.tmp_coords = None
+
+    def on_mouse_release(self, coord, right, left):
+        if (right):
+            if (self.circle.center is None):
+                self.circle.center = coord
+            else:
+                rad = coord.dist(self.circle.center)
+                self.circle.rad = rad
+                return self.save()
+
+    def on_mouse_press(self, coord, right, left):
+        pass
+
+    def on_mouse_move(self, coord):
+        self.tmp_coords = coord
+
+    def on_keyboard(self, *args):
+        pass
+
+    def on_draw(self, ctx):
+        if (self.circle.center is not None and self.circle.rad is None):
+            rad = self.tmp_coords.dist(self.circle.center)
+            self.circle.rad = rad
+            self.circle.draw(ctx)
+            self.circle.rad = None
+
+    def save(self):
+        return self.circle
 
 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
-# from dolist import DoList
 from util import Color, Coords
-from drawables import Line, DrawLine, Polygon, DrawPolygon
+from drawables import DrawLine, DrawPolygon, DrawCircle
 
 
 class Drawer(object):
@@ -14,17 +13,19 @@ class Drawer(object):
 
         self.__primary = Color.Black
         self.__secondary = Color.White
+        self.__fill = True
 
         self.__selected_action = DrawLine
-        self.__action = DrawLine(self.__primary, self.__secondary, True)
+        self.__action = DrawLine(self.__primary, self.__secondary, self.__fill)
 
         self.__init_window()
 
     def __init_window(self):
         self.__window = Gtk.Window()
         w = self.__window
-        w.connect('delete-event', Gtk.main_quit)
+
         w.add_events(Gdk.EventMask.KEY_PRESS_MASK)
+        w.connect('delete-event', Gtk.main_quit)
         w.connect("key-press-event", self.on_keyboard)
         draw_area = Gtk.DrawingArea()
         draw_area.add_events(Gdk.EventMask.BUTTON_PRESS_MASK |
@@ -61,24 +62,51 @@ class Drawer(object):
                                              event.button == Gdk.BUTTON_SECONDARY)
         if (ret is not None):
             self.__doli.append(ret)
-            self.__action = self.__selected_action(self.__primary, self.__secondary, True)
+            self.__action = self.__selected_action(self.__primary, self.__secondary, self.__fill)
 
         self.__draw_area.queue_draw()
 
     def on_mouse_press(self, widget, event):
-        self.__action.on_mouse_press(Coords(event.x, event.y),
+        ret = self.__action.on_mouse_press(Coords(event.x, event.y),
                                      event.button == Gdk.BUTTON_PRIMARY,
                                      event.button == Gdk.BUTTON_SECONDARY)
+        if (ret is not None):
+            self.__doli.append(ret)
+            self.__action = self.__selected_action(self.__primary, self.__secondary, self.__fill)
+
+        self.__draw_area.queue_draw()
+
+    def kill_action(self):
+        self.__action = self.__selected_action(self.__primary,
+                                               self.__secondary,
+                                               self.__fill)
 
     def on_keyboard(self, widget, event):
         if (event.keyval == Gdk.KEY_Escape):
             # Kill the current action
-            self.__action = self.__selected_action(self.__primary, self.__secondary, True)
+            self.kill_action()
 
         if (event.keyval == Gdk.KEY_q):
             Gtk.main_quit()
 
-        if (event.keyval == Gdk.KEY_z and event.state & Gdk.ModifierType.CONTROL_MASK):
+        if (event.keyval == Gdk.KEY_p):
+            self.__selected_action = DrawPolygon
+            self.kill_action()
+
+        if (event.keyval == Gdk.KEY_l):
+            self.__selected_action = DrawLine
+            self.kill_action()
+
+        if (event.keyval == Gdk.KEY_c):
+            self.__selected_action = DrawCircle
+            self.kill_action()
+
+        if (event.keyval == Gdk.KEY_f):
+            self.__fill = not self.__fill
+            self.kill_action()
+
+        if (event.keyval == Gdk.KEY_z and
+            event.state & Gdk.ModifierType.CONTROL_MASK):
             print("<CTRL>z")
             if (len(self.__doli) != 0):
                 del self.__doli[-1]
